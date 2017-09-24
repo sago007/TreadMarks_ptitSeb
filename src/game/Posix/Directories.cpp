969c0619969b6cf61fd1900e19c8a52a61b703e9
sago007 <poul@poulsander.com>
2017-09-24 14:24:39 +0200

    Now uses the XDG specification

--- src/game/Posix/Directories.cpp
+++ src/game/Posix/Directories.cpp
@@ -1,35 +1,48 @@
 // This file is part of Tread Marks
-// 
+//
 // Tread Marks is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Tread Marks is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Tread Marks.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../Directories.h"
 #include "cifm.h"
-#include <sys/stat.h>
+#include <experimental/filesystem>
+
+static void CreateDirectoriesRecursivly(const char* the_path) {
+	std::error_code e;
+	std::experimental::filesystem::create_directories(the_path, e);
+	if (e) {
+		fprintf(stderr, "Failed to create \"%s\". Error: %s\n", the_path, e.message().c_str());
+	}
+}
 
 CStr GetAppDataDir()
 {
-    static char* appdata = NULL;
-    if(!appdata) {
-        appdata = (char*)malloc(4096);
-        strncpy(appdata, getenv("HOME"), 4096);
-        strncat(appdata, "/.TreadMarks", 4096);
-        if(!ci_FileExists(appdata))
-              mkdir(appdata, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        strncat(appdata, "/", 4096);
-    }
-    return CStr(appdata);
+	CStr the_path;
+      // Could also have been XDG_CONFIG_HOME but XDG_DATA_HOME matches Window's AppData better.
+      const char* xdg_data_home = getenv("XDG_DATA_HOME");
+      const char* the_home = getenv("HOME");
+      if (xdg_data_home) {
+            the_path = xdg_data_home;
+            the_path = the_path + CStr("/Tread Marks/");
+      }
+      else {
+            the_path = the_home;
+            the_path = the_path + CStr("/.local/share/Tread Marks/");
+      }
+	CreateDirectoriesRecursivly(the_path.get());
+	return the_path;
 }
+
 
 CStr GetCommonAppDataDir()
 {
